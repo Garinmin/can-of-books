f3d92f3e90f0612ba14561043948f9d33d2528c2
Garinmin <garinmin@gmail.com>
2021-04-26 17:23:04 -0700

    added login auth0

--- src/Login.js
+++ src/Login.js
@@ -7,10 +7,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 function Login() {
    
-    const { 
-      isAuthenticated,
-      loginWithRedirect } = useAuth0();
-    return (
+    const { loginWithRedirect } = useAuth0();
+    return  (
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>Log In</Card.Title>
@@ -22,7 +20,7 @@ function Login() {
         </Card.Body>
       </Card>
     )
-
+ 
 }
 
 export default Login;
