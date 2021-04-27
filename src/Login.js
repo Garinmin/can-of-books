a73c3cad52d9d3805f2074ecaf0104219e279339
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-27 13:43:54 -0700

    Only return a single button

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './Login.css';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -8,18 +7,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 function Login() {
    
     const { loginWithRedirect } = useAuth0();
-    return  (
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Log In</Card.Title>
-          <Card.Text>
-            Click Below to Log In
-          </Card.Text>
-          <Button onClick={() => loginWithRedirect()}>Log In</Button>
-          
-        </Card.Body>
-      </Card>
-    )
+    return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
  
 }
 
