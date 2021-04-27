ceb2f4f338de24c4be1d794f5eefe9b3152a571b
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-27 14:07:53 -0700

    Add redirect to logout

--- src/Logout.js
+++ src/Logout.js
@@ -6,6 +6,10 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 function Logout() {
   const { logout } = useAuth0();
-  return <Button onClick={logout}>Log out</Button>;
+  return <Button onClick={async () => {
+    logout({
+      returnTo: process.env.REACT_APP_REDIRECT_URI
+    });
+  }}>Log out</Button>;
 }
 export default Logout;
