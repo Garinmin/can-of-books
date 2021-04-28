59f4f699180714ab6acbcde92ee4a2e010ced042
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-28 14:51:14 -0700

    Change url to backend

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
 
   getBooks = async (e) => {
     try {
-      const booksUrl = `http://localhost:3000/books/?email=phony@email.com`;
+      const booksUrl = `http://localhost:3001/books/?email=phony@email.com`;
       const response = await axios.get(booksUrl);
 
       this.setState({
