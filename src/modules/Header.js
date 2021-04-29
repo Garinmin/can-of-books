ad1b3fd735db7b00105d5bf26f025e30f6219b75
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-29 14:41:41 -0700

    Move components to module folder

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
-import './Header.css';
+import '../Header.css';
 import { withAuth0 } from '@auth0/auth0-react';
 import Logout from './Logout';
 import Login from './Login';
--- src/modules/Logout.js
+++ src/modules/Logout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import './Login.css';
+import '../Login.css';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function Logout() {
